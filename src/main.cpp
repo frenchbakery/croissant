09f301e6e345cc1f490438b5b7ca3cbbfc169d16
melektron <matteo@elektron.work>
2023-02-08 14:17:20 +0000

    added basic test setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,10 @@
-#include <iostream>
-
-struct coord
-{
-private:
-    int z;
-
-public:
-    int x, y;
-
-    int sum(int _c)
-    {
-        return x + y + _c;
-    }
-};
-
-void myfunc(int _a)
-{
-    std::cout << _a << std::endl;
-}
-
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-
-    coord mylocation;
-
-    std::cout << mylocation.sum(85) << std::endl;
-
-    return 0;
-}
+/**
+ * @file main.cpp
+ * @author melektron
+ * @brief croissant robot main file
+ * @version 0.1
+ * @date 2023-02-08
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
