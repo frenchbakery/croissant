9d32f10f56aedacd8fbb8cfde7e6450522dee7c5
melektron <matteo@elektron.work>
2023-02-21 15:05:14 +0000

    added line tracker files

---
+++ src/drivers/croissant/line_tracker/line_tracker.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file line_tracker.hpp
+ * @author melektron
+ * @brief class to access and control the line tracker function
+ * @version 0.1
+ * @date 2023-02-21
+ * 
+ * @copyright Copyright FrenchBakery (c) 2023
+ * 
+ */
+
+#pragma once
+
+
