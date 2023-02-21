a315f0c4ed78678a73bb378b89224970eceba91d
melektron <matteo@elektron.work>
2023-02-21 14:36:40 +0000

    moved ramped_motor to kiprplus and re-added default include dir

--- src/tests/pomtest.cpp
+++ src/tests/pomtest.cpp
@@ -24,13 +24,20 @@ int main()
 {
     go::pom_sorter.initialize();
     go::nav.initialize();
+    
+    /*go::pom_sorter.setColorSelector(PomSorter::pos_t::red);
 
+    go::nav.driveDistance(35);
+    go::nav.awaitTargetReached();
+    go::nav.rotateBy(-M_PI_2);
+    go::nav.awaitTargetReached();*/
+    
     for (int i = 0; i < 5; i++)
     {
-        go::pom_sorter.setColorSelector(PomSorter::pos_t::red);
+        go::pom_sorter.setColorSelector(PomSorter::pos_t::green);
         go::nav.driveDistance(20);
         go::nav.awaitTargetReached();
-        go::pom_sorter.setColorSelector(PomSorter::pos_t::green);
+        go::pom_sorter.setColorSelector(PomSorter::pos_t::red);
         go::nav.driveDistance(20);
         go::nav.awaitTargetReached();
     }
