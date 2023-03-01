6ccf9c8669949411bc9a4bfcb72ade389cfa3f9f
nilusink <nilusink@protonmail.com>
2023-03-01 19:22:21 +0000

    added functionality and docs

--- src/drivers/croissant/arm/arm.cpp
+++ src/drivers/croissant/arm/arm.cpp
@@ -17,11 +17,81 @@ Arm::Arm(int motor_port, int left_servo_port, int right_servo_port, int end_swit
 
 void Arm::calibrate()
 {
+    // reset servo positions
+    int l_speed = l_servo.getSpeed();
+    int r_speed = r_servo.getSpeed();
+
+    l_servo.setPosition(l_initial, 2048);
+    r_servo.setPosition(r_initial, 2048);
+
     // wait for motor to reach end
     y_motor.moveAtVelocity(-100);
     while (!esw.value()) { msleep(10); };
     y_motor.off();
 
+    // wait a little bit longer so the motor actually stops
+    msleep(200);
+
+    // reset the position counters
     y_motor.clearPositionCounter();
     y_motor.setAbsoluteTarget(0);
+
+    // wait for servos to travel and rollback their speed
+    l_servo.waitUntilComleted();
+    r_servo.waitUntilComleted();
+    l_servo.setSpeed(l_speed);
+    r_servo.setSpeed(r_speed);
+}
+
+
+void Arm::setYPerc(float percent)
+{
+    if (percent > 100 || percent < 0)
+    {
+        std::cout << "Invalid percent value: " << percent << "% when max height is 100%\n";
+        y_motor.off();
+        shutdownAndBlock();
+    }
+    y_motor.setAbsoluteTarget(max_y * (percent / 100));
+}
+
+float Arm::getYPerc()
+{
+    return 100 * (y_motor.getPosition() / max_y);
+}
+
+void Arm::setYcm(float cm)
+{
+    if (cm > y_cm || cm < 0)
+    {
+        std::cout << "Invalid cm value: " << cm << "cm when max height is " << y_cm << "cm\n";
+        shutdownAndBlock();
+    }
+    y_motor.setAbsoluteTarget((cm / y_cm) * max_y);
+}
+
+float Arm::getYcm()
+{
+    return y_cm * (y_motor.getPosition() / max_y);
+}
+
+
+void Arm::shutdownAndBlock()
+{
+    y_motor.off();
+    l_servo.disable();
+    r_servo.disable();
+    for (;;) { msleep(1000); }
+}
+
+
+void Arm::grab(int percent)
+{
+    // move cross
+}
+
+
+void Arm::tilt(int angle)
+{
+    // move straight
 }
--- src/drivers/croissant/arm/arm.hpp
+++ src/drivers/croissant/arm/arm.hpp
@@ -21,8 +21,18 @@ class Arm
         kp::SmoothServo l_servo;
         kp::SmoothServo r_servo;
         kipr::digital::Digital esw;
-        const int max_y = 1350;
 
+        // settings
+        const int max_y = 1350;  // max motor position count
+        const float y_cm = 9.6;  // real life measurement
+        const int l_initial = 600;
+        const int r_initial = 1448;
+
+        /**
+         * @brief turn of all motors / servos and block in an infinite loop
+         * 
+         */
+        void shutdownAndBlock();
 
     public:
         Arm(
@@ -32,6 +42,29 @@ class Arm
             int end_switch_port
         );
 
+        /**
+         * @brief set initial motor position and calibrate range
+         * 
+         */
         void calibrate();
+
+        /**
+         * @brief set the arm height in percent (0...down, 100...up)
+         * 
+         * @param percent value between 0 and 100
+         */
         void setYPerc(float percent);
+        float getYPerc();
+
+        /**
+         * @brief set the arm height in cm (0...down, 9.6...up)
+         * 
+         * @param cm value between 0 and 9.6
+         */
+        void setYcm(float cm);
+        float getYcm();
+
+        void grab(int percent);
+
+        void tilt(int angle);
 };
